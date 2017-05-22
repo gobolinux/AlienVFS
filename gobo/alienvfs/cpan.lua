d30a5c84c89c854c4a31cdab6a7f8383ae1c00b4
Lucas C. Villa Real <lucasvr@gmail.com>
2017-05-22 15:30:04 -0300

    Introduce a new backend: CPAN.

---
+++ gobo/alienvfs/cpan.lua
@@ -0,0 +1,89 @@
+-- AlienVFS: CPAN backend
+-- Written by Lucas C. Villa Real <lucasvr@gobolinux.org>
+-- Released under the GNU GPL version 2
+
+local glob = require "posix.glob"
+
+local cpan = {
+    cpan_dir = nil,
+    packlist_dir = nil,
+    perldoc_output = {},
+
+    getModules = function(self)
+        local modules = {}
+        local f = io.popen("perldoc -t perllocal")
+        for line in f:lines() do
+            self.perldoc_output[#self.perldoc_output + 1] = line
+            if line:find("Module") ~= nil then
+                local name = string.gsub(line, '^.*" ', '')
+                table.insert(modules, name)
+            end
+        end
+        f:close()
+        return modules
+    end,
+
+    getModuleInfo = function(self, module)
+        local watch = false
+        local version = nil
+        local namespace = nil
+        for _,line in pairs(self.perldoc_output) do
+            if line:find(module .. "$") ~= nil then
+                watch = true
+            elseif watch == true then
+                istart, iend = line:find('"installed into: ')
+                if istart ~= nil then
+                    namespace = line:sub(iend+1, -2)
+                else
+                    istart, iend = line:find('"VERSION: ')
+                    if istart ~= nil then
+                        version = line:sub(iend+1, -2)
+                    end
+                end
+            end
+            if version ~= nil and namespace ~= nil then
+                break
+            end
+        end
+        return version, namespace
+    end,
+
+    packListDir = function(self, module)
+        if self.packlist_dir == nil then
+            local arch = io.popen("uname -m"):read("*l")
+            for _,dir in pairs(glob.glob(self.cpan_dir .. "/lib/perl*/" .. arch .. "*/auto")) do
+                self.packlist_dir = dir
+            end
+        end
+        return self.packlist_dir
+    end,
+
+    parsePackList = function(self, module)
+        local filelist = {}
+        local packlist = self:packListDir() .. "/" .. string.gsub(module, "::", "/") .. "/.packlist"
+        local f = io.open(packlist)
+        if f ~= nil then
+            for line in f:lines() do
+                table.insert(filelist, line:sub(self.cpan_dir:len()+2))
+            end
+        end
+        f:close()
+        return filelist
+    end,
+
+    parse = function(self, cpan_dir)
+        self.cpan_dir = cpan_dir
+        local programs = {}
+        for _,module in pairs(self:getModules()) do
+            local program = {}
+            program.name = module
+            program.version = self:getModuleInfo(module)
+            program.namespace = self.cpan_dir
+            program.filelist = self:parsePackList(module)
+            table.insert(programs, program)
+        end
+        return programs
+    end
+}
+
+return cpan
