bf860ed1361f9172f2c0be47cb48cda9f747d083
Lucas C. Villa Real <lucasvr@gmail.com>
2017-05-21 21:22:25 -0300

    First import. This version features support for PIP.

---
+++ PIP.lua
@@ -0,0 +1,105 @@
+#!/usr/bin/lua
+
+-- AlienVFS: PIP backend
+-- Written by Lucas C. Villa Real <lucasvr@gobolinux.org>
+-- Released under the GNU GPL version 2
+
+local lfs = require "lfs"
+local posix = require "posix"
+local lunajson = require "lunajson"
+
+local PIP = {
+    pip_dir = nil,
+
+    readNameVersion = function(self, f)
+        local name = nil
+        local version = nil
+        for line in f:lines() do
+            name_ = line:find("Name: ")
+            version_ = line:find("Version: ")
+            if name_ ~= nil then
+                name = self:getValue(line)
+            end
+            if version_ ~= nil then
+                version = self:getValue(line)
+            end
+        end
+        return name, version
+    end,
+
+    parseEgg = function(self, egg_dir)
+        local program = {}
+        local f = io.open(egg_dir.."/PKG-INFO")
+        if f ~= nil then
+            program.name, program.version = self:readNameVersion(f)
+            f:close()
+        end
+        local f = io.open(egg_dir.."/installed-files.txt")
+        if f ~= nil then
+            program.filelist = {}
+            for line in f:lines() do
+                local fname = egg_dir.."/"..line
+                local path = posix.realpath(fname)
+                if path ~= nil then
+                    table.insert(program.filelist, path:sub(self.pip_dir:len()+2))
+                end
+            end
+            f:close()
+        end
+        return program
+    end,
+
+    getValue = function(self, line)
+        return line:sub(line:len()+2 - line:reverse():find(" "))
+    end,
+
+    parseDistInfo = function(self, dist_dir)
+        local program = {}
+        local f = io.open(dist_dir.."/metadata.json")
+        if f ~= nil then
+            local jsonstr = f:read("*all")
+            local jsondoc = lunajson.decode(jsonstr)
+            program.name = jsondoc.name
+            program.version = jsondoc.version
+            f:close()
+        else
+            f = io.open(dist_dir.."/METADATA")
+            if f ~= nil then
+                program.name, program.version = self:readNameVersion(f)
+                f:close()
+            end
+        end
+        local f = io.open(dist_dir.."/RECORD")
+        if f ~= nil then
+            program.filelist = {}
+            for line in f:lines() do
+                local fname = dist_dir.."/../"..line:sub(1, line:find(",")-1)
+                local path = posix.realpath(fname)
+                if path ~= nil then
+                    table.insert(program.filelist, path:sub(self.pip_dir:len()+2))
+                end
+            end
+            f:close()
+        end
+        return program
+    end,
+
+    parse = function(self, pip_dir)
+        local programs = {}
+        self.pip_dir = pip_dir
+        for file in lfs.dir(pip_dir) do
+            if lfs.attributes(pip_dir.."/"..file, "mode") == "directory" then
+                local fname = pip_dir.."/"..file
+                if string.find(fname, "egg-info", 1, true) ~= nil then
+                    table.insert(programs, self:parseEgg(fname))
+
+                elseif string.find(fname, "dist-info", 1, true) ~= nil then
+                    table.insert(programs, self:parseDistInfo(fname))
+                end
+            end
+        end
+        return programs
+    end
+}
+
+return PIP
---
+++ README.md
@@ -0,0 +1,27 @@
+# AlienVFS
+
+AlienVFS is a virtual filesystem that mirrors modules installed by
+programming language package managers under a centralized directory.
+
+# Example
+
+```bash
+] ls /Mount/AlienVFS
+PIP:agate        PIP:Flask                     PIP:isodate       PIP:parsedatetime    PIP:requests
+PIP:agate-dbf    PIP:Flask-Babel               PIP:itsdangerous  PIP:passlib          PIP:rsa
+PIP:agate-excel  PIP:Flask-Gravatar            PIP:jdcal         PIP:psycopg2         PIP:setuptools
+PIP:agate-sql    PIP:Flask-HTMLmin             PIP:jellyfish     PIP:pyasn1           PIP:Shapely
+PIP:alabaster    PIP:Flask-Login               PIP:Jinja2        PIP:pyasn1-modules   PIP:shellescape
+PIP:appdirs      PIP:Flask-Mail                PIP:lasio         PIP:pycrypto         PIP:simplejson
+PIP:asn1crypto   PIP:Flask-Principal           PIP:leather       PIP:pyDBF            PIP:six
+PIP:Babel        PIP:Flask-Security            PIP:lxml          PIP:Pygments         PIP:snowballstemmer
+PIP:blinker      PIP:Flask-SQLAlchemy          PIP:MarkupSafe    PIP:pyldap           PIP:Sphinx
+PIP:click        PIP:Flask-WTF                 PIP:matplotlib    PIP:PyOpenGL         PIP:SQLAlchemy
+PIP:crypto       PIP:future                    PIP:Naked         PIP:pyparsing        PIP:sqlparse
+PIP:csvkit       PIP:google-api-python-client  PIP:namedlist     PIP:python-aalib     PIP:Unidecode
+PIP:cycler       PIP:htmlmin                   PIP:numpy         PIP:python-dateutil  PIP:uritemplate
+PIP:dbfread      PIP:httplib2                  PIP:oauth2client  PIP:python-slugify   PIP:Werkzeug
+PIP:docutils     PIP:idna                      PIP:openpyxl      PIP:pytimeparse      PIP:WTForms
+PIP:enum34       PIP:imagesize                 PIP:ordereddict   PIP:pytz             PIP:xlrd
+PIP:et-xmlfile   PIP:ipaddress                 PIP:packaging     PIP:PyYAML           PIP:xlwt
+```
